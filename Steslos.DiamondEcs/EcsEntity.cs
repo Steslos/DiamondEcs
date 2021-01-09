aa5c52d2fccfe1418d99b1924f930b7db265cfcd
Steslos <rinon32@yahoo.com>
2021-01-08 18:23:24 -0800

    removed ability for user to create EcsEntity classes, updated documentation of MaximumEntities

--- Steslos.DiamondEcs/EcsEntity.cs
+++ Steslos.DiamondEcs/EcsEntity.cs
@@ -5,8 +5,17 @@
     /// </summary>
     public sealed class EcsEntity
     {
+        /// <summary>
+        /// The maximum amount of entities that can exist at once.
+        /// One entity is reserved for attaching singleton components to; users should subtract one
+        /// from this value for the usable amount of entities available.
+        /// </summary>
         public const int MaximumEntities = 5000;
 
         internal EcsSignature Signature { get; } = new EcsSignature();
+
+        internal EcsEntity()
+        {
+        }
     }
 }
